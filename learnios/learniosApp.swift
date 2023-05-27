cb5a274514fb3c454f99ead93e3ecad5eb816697
Anurag Kandulna <dev.ak@outlook.in>
2023-05-27 11:26:16 +0530

    Initial Commit

---
+++ learnios/learniosApp.swift
@@ -0,0 +1,17 @@
+//
+//  learniosApp.swift
+//  learnios
+//
+//  Created by Anurag Kandulna on 27/05/23.
+//
+
+import SwiftUI
+
+@main
+struct learniosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
