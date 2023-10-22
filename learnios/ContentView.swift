93ae454d1e1563ff45ac7b2a0fcf5b7cc6a61d85
Anurag Kandulna <dev.ak@outlook.in>
2023-10-22 20:50:41 +0530

    test commit

--- learnios/ContentView.swift
+++ learnios/ContentView.swift
@@ -10,9 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                
+            }
+            Image("anurag-pic")
+                .resizable()
+                .frame(width: 250, height: 400)
+                .cornerRadius(20)
+                .imageScale(.small)
+                .padding(20)
             Text("Hello, world!")
         }
         .padding()
---
+++ learnios/basics/language_basics.swift
@@ -0,0 +1,10 @@
+//
+//  language_basics.swift
+//  learnios
+//
+//  Created by Anurag Kandulna on 07/10/23.
+//
+
+//import Foundation
+
+
